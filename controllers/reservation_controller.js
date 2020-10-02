bfaeacc7c36126bc8578148a6a391edd0a42d34d
Filip Sjöberg <filip.sjoberg@hotmail.com>
2020-10-02 09:35:06 +0200

    commit solving problem whit reservation_controller

--- controllers/reservation_controller.js
+++ controllers/reservation_controller.js
@@ -12,43 +12,49 @@ const AVAILABLE_TABLE = 15
  */
 
 
-const availableTable = async (req, res) => {
+const availableTable = async (req, res) =>{
     Reservation.find({
         "date": req.params.date,
     })
-        .then(reservations => {
-            //18.00
-            const firstTime = []
-            //21.00
-            const lastTime = []
-
-            // check if the reservation is 18.00 or 21.00
-            reservations.filter(reservation => {
-                if (reservation.time === "18.00") {
-                    firstTime.push(reservation)
-
-                } else {
-                    lastTime.push(reservation)
+    .then(reservations => {
+        //18.00
+        const firstTime = []
+        //21.00
+        const lastTime = []
+ 
+        // check if the reservation is 18.00 or 21.00
+        reservations.filter( reservation => {
+            if(reservation.time === "18.00"){
+                firstTime.push(reservation)
+                
+            } else {
+                lastTime.push(reservation)
+            }
+        })
+        
+        if(firstTime.length < AVAILABLE_TABLE || lastTime.length < AVAILABLE_TABLE ) {
+            res.send({
+                status: 'success',
+                data: {
+                    "your req": req.params.date,
+                    "avilable_table_18.00 ": AVAILABLE_TABLE - firstTime.length,
+                    "avilable_table_21.00": AVAILABLE_TABLE - lastTime.length
                 }
             })
-
-            if (firstTime.length < AVAILABLE_TABLE || lastTime.length < AVAILABLE_TABLE) {
-                res.send({
-                    status: 'success',
-                    data: {
-                        "your req": req.params.date,
-                        "avilable_table_18.00 ": AVAILABLE_TABLE - firstTime.length,
-                        "avilable_table_21.00": AVAILABLE_TABLE - lastTime.length
-                    }
-                })
-            } else {
-                res.send({
-                    status: 'fail',
-                    message: 'no availvle table at this day',
-                    available: false,
-                })
+        } else{
+            res.send({
+                status: 'fail',
+                message: 'no availvle table at this day',
+                available: false,
             })
-}
+        }
+    }).catch(err => {
+        res.status(500).send({
+            status: 'fail',
+            message: 'Exception thrown when trying to find table at:' + req.params.date
+        })
+    })
+ }
 
 
 /**
